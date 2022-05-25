62c8074e9587186db7a813aac0766b69267c7c30
SheilaNS <shei.nsantos@gmail.com>
2022-05-25 18:33:26 -0300

    [FEAT] Requisito 16 - feito

--- desafio16.sql
+++ desafio16.sql
@@ -1,2 +1,2 @@
-SELECT id, supplier_id FROM northwind.purchase_orders
-WHERE supplier_id IN (1, 3, 5, 7);
+SELECT submitted_date FROM northwind.purchase_orders
+WHERE submitted_date BETWEEN '2006-01-26 00:00:00' AND '2006-03-31 23:59:59';
